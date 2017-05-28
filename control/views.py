993c14bb833ff20d1edec3648e03647be80e7d44
acer <18202121195@sina.cn>
2017-05-28 15:50:21 +0800

    add command save function

--- control/views.py
+++ control/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+import os
 
 def index(request):
     return render(request, 'control.html')
 
 def add(request):
     a = request.GET.get('a', None)
-    b = request.GET.get('b', None)
-    a = 6
-    b = int(b)
-    return HttpResponse(str(a))
+    a = str(a)
+    filrpath = settings.MEDIA_ROOT + 'command.txt'
+    try:
+        f = open(filrpath, 'w')
+        f.write(a)
+    finally:
+        if f:
+            f.close()
+
+    return render(request, 'control.html')
+
